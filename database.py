ea7d1d3fc430691feeb17e7e7c96ce137e48fa7f
asarj <17691947+asarj@users.noreply.github.com>
2018-02-03 15:42:25 -0500

    finished changes

--- database.py
+++ database.py
@@ -7,16 +7,16 @@ class Database(object):
 
     def __init__(self, name):
         self._name = name
-    
+
     def _execute(self, command, args=None):
-        connection = sqlite3.connect("/home/shaan/cs/projects/HackHealth/hack.db")
+        connection = sqlite3.connect("/Users/jujubear/Documents/OfficialHackHealth/HackHealth/hack.db")
         cursor = connection.cursor()
 
         if args is None:
             out = cursor.execute(command).fetchall()
         else:
             out = cursor.execute(command, args).fetchall()
-        
+
         connection.commit()
         connection.close()
         return out
