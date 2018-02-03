0bc91b0d6ea27fd24da89aa5aefe813f4307d63c
thefireblade <jhuang6625@bths.edu>
2018-02-03 13:08:22 -0500

    Added a addUser option

--- dbaccess.py
+++ dbaccess.py
@@ -4,8 +4,8 @@ __all__ = ['AuthDatabase']
 
 class AuthDatabase(Database):
 	def addLog(self,userid,date,calories):
-		self._execute("INSERT INTO FoodData(userId,calorieCount,transactionDate) VALUES(?,?,?);",(userid,date,calories))
+		self._execute("INSERT INTO FoodData(userId, calorieCount, transactionDate) VALUES(?,?,?);",(userid,calories,date))
 	def addUser(self, userid,name,calories):
-		self._execute("INSERT INTO FoodData(userId,calorieCount,transactionDate) VALUES(?,?,?);",(userid,date,calories))
+		self._execute("INSERT INTO FoodData(userId, name, calorieTarget, currentWeight, lastTransaction) VALUES(?,?,?,?,?);",(userid, name, calories, weight, transaction))
 
 
